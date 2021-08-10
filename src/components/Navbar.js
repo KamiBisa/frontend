cbf54ae411720573637e607d89ee9e1171a76221
robertwija1033 <robertwijaya1033@gmail.com>
2021-08-10 14:39:59 +0700

    add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,28 @@
+import { Box, Image, Img, Text, Stack, UnorderedList } from '@chakra-ui/react';
 import React from 'react';
-
-export default function Navbar() {
-  return <></>;
+// import "./Navbar.css";
+const Navbar = () => {
+    return (
+        <>
+            <Box color="black" fontSize="20px" >
+                <Image />
+                <Stack isInline spacing="10"  >
+                    <Box>
+                        <Text>KamiBisa</Text>
+                        <Text>Donasikan</Text>
+                        <Text>Ajukan Campaign</Text>
+                        <Text>Transparansi</Text>
+                        <Text>#covid19</Text>
+                        <Text>FAQ</Text>
+                        <Text>Tentang Kami</Text>
+                        <Text>Masuk</Text>
+                        <Text>Daftar</Text>
+                        <Text>Language</Text>
+                    </Box>
+                </Stack>
+            </Box>
+        </>
+    );
 }
+
+export default Navbar;
