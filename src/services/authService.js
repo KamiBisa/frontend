4db728676f82a3a8ae7caf00633d4c278e8f387e
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-08-12 16:48:57 +0800

    update authService backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,20 +3,26 @@ import crypto from 'crypto-js'
 import { API_URL } from '../config/api'
 
 class AuthService {
-  async login(email, password) {
+  async login(username, password) {
     return await axios
-      .post(`${API_URL}/auth/login`, {
-        email,
+      .post(`${API_URL}/api/postLogin`, {
+        username,
         password,
       })
-      .then(response => response.data)
+      .then(response => {
+        const secret = crypto.AES.encrypt(
+          JSON.stringify(response.data),
+          process.env.REACT_APP_ENCRYPT
+        ).toString()
+        localStorage.setItem('user', secret)
+      })
       .catch(err => err)
   }
 
-  async register(email, password) {
+  async register(username, password) {
     return await axios
-      .post(`${API_URL}/auth/register`, {
-        email,
+      .post(`${API_URL}/api/postRegister`, {
+        username,
         password,
       })
       .then(response => response.data)
