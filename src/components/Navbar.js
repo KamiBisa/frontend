4d17e54b69c279002da345a42cb1a2d654ed77a8
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-08-15 16:43:15 +0800

    adjust navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,20 @@
 import {
   Box,
   Image,
-  Img,
   Text,
   Stack,
-  UnorderedList,
-  Link,
   Input,
-  border,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  Button,
 } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { useHistory } from 'react-router-dom'
 
-const NavbarItem = ({ children, ...props }) => {
-  return (
-    <Box
-      w={{ base: '95%', md: '100px' }}
-      h="40px"
-      _hover={{
-        md: { bgColor: '#F1f1f1' },
-      }}
-      textAlign={{ md: 'center' }}
-      color="#45545b!important"
-      boxShadow={{ base: ' 0 1px 0px rgba(0,0,0,.1)', md: 'none' }}
-      {...props}
-    >
-      <Link src="#" fontFamily="helvetica" _hover={{ textDecoration: 'none' }}>
-        <Text>{children}</Text>
-      </Link>
-    </Box>
-  )
-}
-
 const Navbar = () => {
-  const [clickBar, setClickBar] = useState(false)
-
   const history = useHistory()
   return (
     <>
@@ -55,14 +30,24 @@ const Navbar = () => {
           alignItems="center"
           justifyContent="space-around"
         >
+          <Text
+            fontSize="xl"
+            fontWeight="bold"
+            onClick={() => history.push('/')}
+            color="blue.500"
+            d={{ base: 'none', md: 'block' }}
+            cursor="pointer"
+          >
+            KamiBisa
+          </Text>
           <Box
             w="60%"
-            bgColor="black"
             display="flex"
             justifyContent="flex-end"
             borderRadius="100px"
             h="45px"
             bgColor="#EAF3F9"
+            alignItems="center"
           >
             <Input
               placeholder="Ingin bantu siapa hari ini ? "
@@ -91,44 +76,21 @@ const Navbar = () => {
               </MenuList>
             </Menu>
           </Box>
-          <Box
-            w={{ base: '93%', md: 'auto' }}
-            position={{ base: 'fixed', md: 'unset' }}
-            bgColor="white"
-            left="3.5%"
-            borderRadius="10px"
-            zIndex="1"
-            transition="0.5s"
-            h={{ base: clickBar ? 'auto' : '0', md: '0px' }}
-            boxShadow="0 0 5px rgba(0,0,0,.3)"
-            marginTop={{ base: '270px', md: '40px' }}
-            d={{ base: clickBar ? 'block' : 'absolute', md: 'flex' }}
-            flexDirection={{ md: 'row-reverse' }}
+
+          <Button
+            variant="ghost"
+            d={{ base: 'none', md: 'block' }}
+            onClick={() => history.push('/login')}
           >
-            <Stack
-              d={{ base: clickBar ? 'flex' : 'none', md: 'flex' }}
-              transition="2s"
-              alignItems="flex-end"
-              flexDirection={{ base: 'column', md: 'row-reverse' }}
-            >
-              <NavbarItem
-                borderRadius={{ md: '8px' }}
-                onClick={() => history.push('./login')}
-              >
-                Sign In
-              </NavbarItem>
-              <NavbarItem
-                _hover={{
-                  base: { color: 'white', bgColor: 'white', border: 'none' },
-                  md: { bgColor: '#F1f1f1' },
-                }}
-                borderRadius="8px"
-                onClick={() => history.push('./register')}
-              >
-                Register
-              </NavbarItem>
-            </Stack>
-          </Box>
+            Sign In
+          </Button>
+          <Button
+            variant="ghost"
+            d={{ base: 'none', md: 'block' }}
+            onClick={() => history.push('/register')}
+          >
+            Register
+          </Button>
         </Box>
       </Box>
     </>
